9879b04ebcbdd9c8bd95d2a1598528cefab72da1
João Esteves <jesteves@kaizen.com>
2026-02-12 19:59:10 +0000

    markdown support

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,8 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    file_types = { 'markdown' },
+  },
+  config = function(_, opts) require('render-markdown').setup(opts) end,
+}
